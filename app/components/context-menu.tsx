1c171a16b023d50fd13e65052331b37c9064262a
Ethan Chien <liangtie.qian@gmail.com>
2025-03-16 17:24:48 +0800

    feat: add context menu options for BOM and netlist

--- app/components/context-menu.tsx
+++ app/components/context-menu.tsx
@@ -7,14 +7,16 @@ import GitIcon from "../icons/git.svg";
 import TerminalIcon from "../icons/terminal.svg";
 import ErrorIcon from "../icons/error.svg";
 import GlobeIcon from "../icons/globe.svg";
+import { ContextMenuOption } from "./context-menu-option";
 
 interface ContextMenuItem {
   id: string;
   name: string;
   path?: string;
   icon?: React.ReactNode;
-  type: "file" | "section";
+  type: "menu" | "action" | "file" | ContextMenuOption;
   hasChildren?: boolean;
+  children?: ContextMenuItem[];
 }
 
 interface Props {
@@ -33,49 +35,61 @@ const defaultSections: ContextMenuItem[] = [
     id: "schematic",
     name: "Schematic",
     icon: <CircuitIcon />,
-    type: "section",
+    type: "menu",
     hasChildren: true,
+    children: [
+      {
+        id: "bom",
+        name: "bom",
+        type: ContextMenuOption.BOM,
+      },
+      {
+        id: "netlist",
+        name: "netlist",
+        type: ContextMenuOption.NETLIST,
+      },
+    ],
   },
   {
     id: "symbol",
     name: "Symbol",
     icon: <SymbolIcon />,
-    type: "section",
+    type: "menu",
     hasChildren: true,
   },
   {
     id: "docs",
     name: "Docs",
     icon: <BookIcon />,
-    type: "section",
+    type: "menu",
     hasChildren: true,
   },
   {
     id: "git",
     name: "Git",
     icon: <GitIcon />,
-    type: "section",
+    type: "menu",
     hasChildren: true,
   },
   {
     id: "terminals",
     name: "Terminals",
     icon: <TerminalIcon />,
-    type: "section",
+    type: "menu",
     hasChildren: true,
   },
   {
     id: "errors",
     name: "Errors",
     icon: <ErrorIcon />,
-    type: "section",
+    type: "menu",
     hasChildren: true,
   },
   {
     id: "web",
     name: "Web",
     icon: <GlobeIcon />,
-    type: "section",
+    type: "action",
   },
 ];
 
